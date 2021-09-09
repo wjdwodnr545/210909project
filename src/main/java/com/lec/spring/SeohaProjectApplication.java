842a028e844815527cae020759b6182ca4c744ba
baeseoha <bha6050@naver.com>
2021-09-09 21:26:32 +0900

    첫번째 커밋

---
+++ src/main/java/com/lec/spring/SeohaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lec.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeohaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeohaProjectApplication.class, args);
+	}
+
+}
